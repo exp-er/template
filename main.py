a236999c22099ac736120e9166a73e958f758081
Akshat <akshat.pande@outlook.com>
2023-07-18 16:59:26 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("Streamlit Template Gallery")
+st.caption("Includes premade template from documentation and other sources for specific use cases.")
+
+st.header("Section 1")
+st.sidebar.text("Sections")
+st.sidebar.markdown("[Section 1](#section-1) ")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
+st.sidebar.markdown("[Section 1](#section-1)")
